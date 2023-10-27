2ecfc00b3ce533f8c4c8457b7cd7b7deb4789be8
하세곳 <csw020106@naver.com>
2023-10-27 13:35:43 +0000

    test

---
+++ JavaScript/JavaScript Fundamentals - Part 1/JavaScript.js
@@ -0,0 +1,23 @@
+
+// 구름ide자체에선 콘솔창이 안열여서 새로운 창을 열고 콘솔창에 실행시켜 공부함.
+
+/*    Hello Wordl! 
+> alert("hello World")
+<- undefined
+> let js = 'amazing'
+<- undefined
+> if(js === 'amazing') alert("JavaScript is Fun")
+<- undefined
+> js = 'boring'
+<- 'boring'
+> if(js === 'amazing') alert("JavaScript is Fun")
+<- undefined
+> 40 + 8 + 23 - 10
+<- 61
+*/
+
+let massMarks = 78, heightMark = 169, massJohn = 92, heightJohn = 195;
+let MBI = mass 
+
+
+
